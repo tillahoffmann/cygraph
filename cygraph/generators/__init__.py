92b3788218fc5449d73e1fbe88ee30b3f6285b9e
Till Hoffmann <tillahoffmann@gmail.com>
2022-06-06 14:04:09 -0400

    Add `__all__` for improved documentation.

--- cygraph/generators/__init__.py
+++ cygraph/generators/__init__.py
@@ -4,3 +4,13 @@ from .gnp_random_graph import gnp_random_graph  # noqa: F401
 from .redirection_graph import redirection_graph  # noqa: F401
 from .surfer_graph import surfer_graph  # noqa: F401
 from .util import get_random_engine, RandomEngine  # noqa: F401
+
+__all__ = [
+    "duplication_complementation_graph",
+    "duplication_mutation_graph",
+    "gnp_random_graph",
+    "redirection_graph",
+    "surfer_graph",
+    "get_random_engine",
+    "RandomEngine",
+]
